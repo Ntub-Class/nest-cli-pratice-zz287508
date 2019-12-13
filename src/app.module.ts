7ca6b64764d01df82c559fd0691c1b6a8beb5b2e
Your Name <you@example.com>
2019-12-13 19:08:37 +0800

    OK

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { StudentModule } from './student/student.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [NewsModule, StudentModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
